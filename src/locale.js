7c0a7f1f5b82ef1a7860657c43f4726c0b27d00a
dr3w <mail2dr3w@gmail.com>
2016-12-15 00:12:31 +0200

    Fat fingers remedy vol.2

--- src/locale.js
+++ src/locale.js
@@ -4,7 +4,7 @@ module.exports = {
     CURRENT_TAGS_INFO: 'You already have following tags:\n{{tags}}\n\n',
     TYPE_NEW_TAGS_INFO: 'Type in new tags for this sticker (space separated, emoji also work {{emoji}})\n\n',
     DELETE_INFO: '/delete - remove all tags for this sticker\n\n',
-    CANCEL_INFO: '/cancel - if you\'re a quitter',
+    CANCEL_INFO: '/cancel - if you\'re a quitter\n',
     CANCELED: '⛔️ Why nothing? Ok then... ¯\\_(ツ)_/¯',
     DELETED: '💥 It\'s gone!',
     SAVED: '✅ Wow! Much amaze! So creative!'
