22f609d6f04268357d0d61aa332bf9449fea508f
dr3w <mail2dr3w@gmail.com>
2016-12-15 12:51:51 +0200

    Heroku config

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    token: '258025041:AAHE-xLxlDRfr26iYtuRM5aKNtWDcwSQywo',
+    webhook: 'https://telegram-sticker-tag-bot.herokuapp.com',
+    port: 51438,
+
+    firebase: {
+        apiKey: "AIzaSyDpd4Zvn82MZqPKaWxT9cW4hGZOqf7BPtg",
+        authDomain: "telegramstickertagbot-8e833.firebaseapp.com",
+        databaseURL: "https://telegramstickertagbot-8e833.firebaseio.com",
+        storageBucket: "telegramstickertagbot-8e833.appspot.com",
+        messagingSenderId: "47687004678"
+    }
+}
+
