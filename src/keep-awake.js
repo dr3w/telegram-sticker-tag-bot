892480344d63131491b8a9daf99a1c9bca21995f
dr3w <mail2dr3w@gmail.com>
2016-12-29 17:25:28 +0200

    Use post

--- src/keep-awake.js
+++ src/keep-awake.js
@@ -1,14 +1,19 @@
-const https = require('https')
+const request = require('request')
 const cfg = require('../config')
 
 module.exports = function () {
-    "use strict";
+    "use strict"
 
-    console.log('Start keep-awake...');
+    console.log('Start keep-awake...')
 
     setInterval(function () {
-        console.log('poll:', cfg.webhook);
+        const url = cfg.webhook + "/258025041:AAHE-xLxlDRfr26iYtuRM5aKNtWDcwSQywo"
 
-        https.get(cfg.webhook);
+        console.log('poll:', url);
+
+        request.post(url, {}, (error, response, body) => {
+            console.log('poll result:', error, response, body);
+        })
+
     }, 300000); // every 5 minutes (300000)
 }
