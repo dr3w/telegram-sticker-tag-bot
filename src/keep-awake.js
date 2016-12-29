da8568a4aac03fb31ef0e111cc31b625306d2f51
dr3w <mail2dr3w@gmail.com>
2016-12-29 15:59:55 +0200

    Add keep-awake service; updates

--- src/keep-awake.js
+++ src/keep-awake.js
@@ -4,7 +4,11 @@ const cfg = require('../config')
 module.exports = function () {
     "use strict";
 
+    console.log('Start keep-awake...');
+
     setInterval(function () {
+        console.log('poll:', cfg.webhook);
+
         http.get(cfg.webhook);
     }, 300000); // every 5 minutes (300000)
 }
