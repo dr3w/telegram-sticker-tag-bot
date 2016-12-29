140034ee4cb422b625d69f5b52bfe31e3ddfd76a
dr3w <mail2dr3w@gmail.com>
2016-12-29 18:56:52 +0200

    Update package json, keep-awake

--- src/keep-awake.js
+++ src/keep-awake.js
@@ -1,4 +1,4 @@
-const request = require('request')
+const https = require('https')
 const cfg = require('../config')
 
 module.exports = function () {
@@ -7,13 +7,8 @@ module.exports = function () {
     console.log('Start keep-awake...')
 
     setInterval(function () {
-        const url = cfg.webhook + "/258025041:AAHE-xLxlDRfr26iYtuRM5aKNtWDcwSQywo"
+        console.log('poll:', cfg.webhook);
 
-        console.log('poll:', url);
-
-        request.post(url, {}, (error, response, body) => {
-            console.log('poll result:', error, response, body);
-        })
-
-    }, 300000); // every 5 minutes (300000)
+        https.get(cfg.webhook);
+    }, 1000 * 60 * 5); // every 5 minutes (300000)
 }
